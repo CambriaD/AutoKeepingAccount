475c791d0d243913b2d9697f08f6817b19dd600f
CambriaD <835014916@qq.com>
2021-11-01 15:38:01 +0800

    temp

--- app/src/main/java/xyz/cambria/autokeepingaccountbeta/activity/KARecActivity.java
+++ app/src/main/java/xyz/cambria/autokeepingaccountbeta/activity/KARecActivity.java
@@ -2,11 +2,13 @@ package xyz.cambria.autokeepingaccountbeta.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import xyz.cambria.autokeepingaccountbeta.R;
 import xyz.cambria.autokeepingaccountbeta.entity.KeepingAccountRec;
 
-import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class KARecActivity extends AppCompatActivity {
 
@@ -17,8 +19,11 @@ public class KARecActivity extends AppCompatActivity {
         KeepingAccountRec item = (KeepingAccountRec) getIntent().getSerializableExtra("KARItem");
         int index = getIntent().getIntExtra("index" , -1);
 
-        TextView io = findViewById(R.id.textView_io_item_ka_test);
-        bind
+        ((TextView)findViewById(R.id.textView_io_activity_k_a_rec)).setText(item.getOutcomeAccount().equals("您")?"支出":"收入");
+        ((TextView)findViewById(R.id.textView_category_activity_k_a_rec)).setText(item.getCategory().toString());
+        ((TextView)findViewById(R.id.textView_amount_activity_k_a_rec)).setText(item.getAmount().toString());
+        ((TextView)findViewById(R.id.textView_time_activity_k_a_rec)).setText(new SimpleDateFormat("yyyy-MM-ss HH:mm:ss").format(new Date(item.getTime())));
+        ((TextView)findViewById(R.id.textView_balance_activity_k_a_rec)).setText(item.getBalance().toString());
 
     }
 }
